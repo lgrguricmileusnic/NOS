626f751da853ce7b3416159b106804b52d8e7315
Lovro Grgurić Mileusnić <lovrogm@gmail.com>
2022-12-19 23:31:44 +0000

    lab2b gotov

--- lab2b/shofer.c
+++ lab2b/shofer.c
@@ -248,10 +248,16 @@ static int shofer_open_read(struct inode *inode, struct file *filp)
 	return 0;
 }
 
+
 /* open for input_dev */
 static int shofer_open_write(struct inode *inode, struct file *filp)
 {
-	/* todo (similar to shofer_open_read) */
+	struct shofer_dev *shofer;
+	shofer = container_of(inode->i_cdev, struct shofer_dev, cdev);
+	filp->private_data = shofer;
+
+	if ( (filp->f_flags & O_ACCMODE) != O_WRONLY)
+		return -EPERM;
 
 	return 0;
 }
@@ -269,7 +275,7 @@ static ssize_t shofer_read(struct file *filp, char __user *ubuf, size_t count,
 	spin_lock(&out_buff->key);
 
 	dump_buffer("out_dev-end:out_buff:", out_buff);
-
+	
 	retval = kfifo_to_user(fifo, (char __user *) ubuf, count, &copied);
 	if (retval)
 		klog(KERN_WARNING, "kfifo_to_user failed\n");
@@ -287,14 +293,31 @@ static ssize_t shofer_read(struct file *filp, char __user *ubuf, size_t count,
 static ssize_t shofer_write(struct file *filp, const char __user *ubuf,
 	size_t count, loff_t *f_pos)
 {
-	/* todo (similar to read) */
+	ssize_t retval = 0;
+	struct shofer_dev *shofer = filp->private_data;
+	struct buffer *in_buff = shofer->in_buff;
+	struct kfifo *fifo = &in_buff->fifo;
+	unsigned int copied;
 
-	return count;
+	spin_lock(&in_buff->key);
+
+	dump_buffer("in_dev-end:in_buff:", in_buff);
+
+	retval = kfifo_from_user(fifo, (char __user *) ubuf, count, &copied);
+	if (retval)
+		klog(KERN_WARNING, "kfifo_from_user failed\n");
+	else
+		retval = copied;
+
+	dump_buffer("in_dev-end:in_buff:", in_buff);
+
+	spin_unlock(&in_buff->key);
+
+	return retval;
 }
 
 static long control_ioctl (struct file *filp, unsigned int cmd, unsigned long arg)
 {
-	ssize_t retval = 0;
 	struct shofer_dev *shofer = filp->private_data;
 	struct buffer *in_buff = shofer->in_buff;
 	struct buffer *out_buff = shofer->out_buff;
@@ -302,6 +325,8 @@ static long control_ioctl (struct file *filp, unsigned int cmd, unsigned long ar
 	struct kfifo *fifo_out = &out_buff->fifo;
 	char c;
 	int got;
+	int i;
+
 
 	if (!cmd)
 		return -EINVAL;
@@ -309,7 +334,45 @@ static long control_ioctl (struct file *filp, unsigned int cmd, unsigned long ar
 	/* copy cmd bytes from in_buff to out_buff */
 	/* todo (similar to timer) */
 
-	return retval;
+	/* get locks on both buffers */
+	spin_lock(&out_buff->key);
+	spin_lock(&in_buff->key);
+
+	dump_buffer("ioctl-start:in_buff", in_buff);
+	dump_buffer("ioctl-start:out_buff", out_buff);
+	LOG("ioctl moving %u characters", cmd);
+	for (i = 0; i < cmd; i++)
+	{
+		if (kfifo_len(fifo_in) > 0 && kfifo_avail(fifo_out) > 0) {
+			got = kfifo_get(fifo_in, &c);
+			if (got > 0) {
+				got = kfifo_put(fifo_out, c);
+				if (got)
+					LOG("ioctl moved '%c' from in to out", c);
+				else {
+					/* should't happen! */
+					klog(KERN_WARNING, "kfifo_put failed\n");
+					break;
+				}
+			}
+			else { /* should't happen! */
+				klog(KERN_WARNING, "kfifo_get failed\n");
+				break;
+			}
+		}
+		else {
+			LOG("ioctl: nothing in input buffer");
+			break;
+		}
+	}
+
+	dump_buffer("ioctl-end:in_buff", in_buff);
+	dump_buffer("ioctl-end:out_buff", out_buff);
+
+	spin_unlock(&in_buff->key);
+	spin_unlock(&out_buff->key);
+
+	return i;
 }
 
 static void timer_function(struct timer_list *t)
